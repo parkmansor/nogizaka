e88922fe8a2e88e58fc3d38aea5d747d9732af70
parkmansor <57488489+parkmansor@users.noreply.github.com>
2020-08-15 20:29:05 +0900

    Delete nogi-member.ts

--- src/app/nogi-member.ts
+++
@@ -1,9 +0,0 @@
-export interface NogiMemberInfo {
-  name: string;
-  birthday: string
-  home: string
-  bloodType: 'A' | 'O' | 'B' | 'AB' | 'U'
-  height: number
-  generate: number
-  graduate: string
-}
